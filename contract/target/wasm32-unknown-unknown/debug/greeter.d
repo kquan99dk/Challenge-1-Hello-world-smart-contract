ab2f94ac45921309c5131a2700e6360893fffd83
kquan <kquan99dk@gmail.com>
2022-05-26 21:15:43 +0700

    update

---
+++ contract/target/wasm32-unknown-unknown/debug/greeter.d
@@ -0,0 +1,1 @@
+/home/kieuquan/Src/test/test-2/HelloWorld/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /home/kieuquan/Src/test/test-2/HelloWorld/contract/src/lib.rs
---
+++ contract/target/wasm32-unknown-unknown/debug/libgreeter.d
@@ -0,0 +1,1 @@
+/home/kieuquan/Src/test/test-2/HelloWorld/contract/target/wasm32-unknown-unknown/debug/libgreeter.rlib: /home/kieuquan/Src/test/test-2/HelloWorld/contract/src/lib.rs
