4ea876fbc1552fa812dd40c1c30c6281ee8950f0
kquan <kquan99dk@gmail.com>
2022-05-27 22:39:54 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import getConfig from "./config"
 import SignedOut from "./components/SignedOut.vue"
 import SignedIn from "./components/SignedIn.vue"
 
-const nearConfig = getConfig("testnet")
+const nearConfig = getConfig(process.env.NODE_ENV || 'development')
 console.log(
   `networkId:${nearConfig.networkId} CONTRACT_NAME:${nearConfig.contractName}`
 )
--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,9 @@ import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
 import getConfig from './config'
 
 // eslint-disable-next-line no-undef
-// const nearConfig = getConfig(process.env.NODE_ENV || 'development')
-const nearConfig = getConfig('testnet')
+const nearConfig = getConfig(process.env.NODE_ENV || 'development')
+// const nearConfig = getConfig('testnet')
 
-console.log(nearConfig)
 
 // Initialize contract & set global variables
 export async function initContract() {
